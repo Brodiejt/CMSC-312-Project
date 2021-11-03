30fe6a10a05c42fe6dc9cd71e0b1f545e94e7579
Brodiejt <91982046+Brodiejt@users.noreply.github.com>
2021-11-03 04:34:26 -0400

    Add files via upload

--- OS_Simulator/src/kernel/PCB.java
+++ OS_Simulator/src/kernel/PCB.java
@@ -1,4 +1,5 @@
 package kernel;
+
 import java.io.*;
 
 public class PCB {
@@ -10,8 +11,8 @@ public class PCB {
 	static int counter = 0;
 	long start;
 	long end;
-	
-	//PCB constructor
+
+	// PCB constructor
 	public PCB() {
 		pid = counter;
 		counter++;
@@ -19,7 +20,7 @@ public class PCB {
 		programSize = 0;
 		state = 0;
 		programStart = new int[30];
-		start  = 0;
+		start = 0;
 	}
 
 	public void printStatus() {
@@ -31,153 +32,152 @@ public class PCB {
 		System.out.println("");
 	}
 
-public void loadToMemory( File p) throws IOException {
-	File file = p;
-	 BufferedReader br = new BufferedReader(new FileReader(file));
-	 
-	 String st;
-	 int index = 0;
-	 
-	 while((st = br.readLine()) != null) {
-		 this.programStart[index] = generate_opcode(st);
-		 index++;
-	 }
-	 
-	
-	return;
-}
+	// function to parse template or .txt file and generate a file and load each
+	// opcode into an array of integers simulating
+	// memory for executuon
+	public void loadToMemory(File p) throws IOException {
+		File file = p;
+		BufferedReader br = new BufferedReader(new FileReader(file));
 
-public int generate_opcode(String line) {
-	int code = 0;
-    char ch1 = line.charAt(0);
-    switch(ch1){
-    case 'c':
-        code = 10000;
-        code += (line.charAt(10) - '0')*1000;
-        code += (line.charAt(11) - '0')*100;
-        code += (line.charAt(13) - '0')*10;
-        code += (line.charAt(14) - '0');
-        break;
-    case 'I':
-        code = 20000;
-        break;
-    case 'F':
-        code = 40000;
-        break;
-    case 's':
-    	code += 50000;
-    	if(line.charAt(7)=='1'){code+=1;}
+		String st;
+		int index = 0;
 
-    }
-	
-	return code;
-}
+		while ((st = br.readLine()) != null) {
+			this.programStart[index] = generate_opcode(st);
+			index++;
+		}
 
-public int execute(int time, Queue qR, Queue qJ, Queue qIO, Semaphore semaph) throws InterruptedException {
-	
-	long start = System.currentTimeMillis();
-	
-	if (this.start == 0) {
-		this.start = System.currentTimeMillis();
+		return;
 	}
-	// ...
-	
-	int operation = 0;
-	System.out.println("Executing Process "+this.pid);
-	
-	this.state = 2; // running state
-	
-	start:
-	for(int i=this.pc;programStart[i]!=0;i++) {
-		operation = this.programStart[i] / 10000;
-		switch(operation){
-        //"calculate" Opcode
-    case 1:
-        Thread.sleep(1); //sleep 1 second
-        break;
 
-    //"I/O" Opcode
-    case 2:
-        this.state = 3;
-        this.pc = i+1;
-        qIO.queueEnqueue(this);
-        System.out.println("Process with pid:"+ this.pid + " WAITING\n" );
-        return(1);
-        
+// function to generate a specifc opcode with a given string. First digit of opcode is the type of instruction (calculate, fork, I/O, Critical Section)
+	public int generate_opcode(String line) {
+		int code = 0;
+		char ch1 = line.charAt(0);
+		switch (ch1) {
+		case 'c':
+			code = 10000;
+			code += (line.charAt(10) - '0') * 1000;
+			code += (line.charAt(11) - '0') * 100;
+			code += (line.charAt(13) - '0') * 10;
+			code += (line.charAt(14) - '0');
+			break;
+		case 'I':
+			code = 20000;
+			break;
+		case 'F':
+			code = 40000;
+			break;
+		case 's':
+			code += 50000;
+			if (line.charAt(7) == '1') {
+				code += 1;
+			}
 
-      //Criitical section entered  
-    case 5:
-    	//System.out.println("Process: "+ this.pid+ "sh");
-    	
-    		switch(this.programStart[i]%10){
-    		
-    			case 0:
-    				System.out.println("Process: "+ this.pid+ " Entering Critical Section; " );
-    				//Semaphore.queue.add(this.pid);
-    				//this.pc = i++;
-    				//if(Semaphore.numUsed==Semaphore.numAllowed) {
-    					//Semaphore.mutexLock = true;
-    				//System.out.println("MAX Critical Section reached");	
-    				
-    				break;
-    			
-    			case 1:
-    				//Semaphore.numUsed--;
-    				//if(Semaphore.numUsed<Semaphore.numAllowed) Semaphore.mutexLock = false;
-    				System.out.println("Process: "+ this.pid+ " Leaving Critical Section" );
-    				break;
-    			
-    		}
-    		
-    	
-    	
-        //
+		}
 
-        //Fork opcode
-    case 4:
-        this.state = 3;
-        this.pc = i+1;
-        qR.queueEnqueue(this);
-        return (2);
-        //generate process and execute it
-
-    }
-		
-    
-	long finish = System.currentTimeMillis();
-	long timeElapsed = finish - start;
-	this.end = System.currentTimeMillis();
-	
-	
-	
-    //System.out.println("time elapsed:" + timeElapsed);
-    
-    //if timer runs out, save pc, change state to ready,  enqueue current PCB to ready queue
-    if((timeElapsed) > time){
-    	this.pc++;
-    	this.state = 2;
-    	qR.queueEnqueue(this);
-    	return 2;
-        
-    }
-}
-main m = new main();
-System.out.println("Process with pid:"+ this.pid + " terminated\n" );
-
-main.turnarounds.add((int) (this.end-this.start));
-
-this.state = 5;
-
-return 0;
-		
-}
-
-public void printMemory() {
-	System.out.println("Printing Memory for PID: "+this.pid);
-	for(int i=0;this.programStart[i] !=0;i++ ) {
-		System.out.println(programStart[i]);
+		return code;
 	}
-	return;
 
-}
+//Function to simulate execution of a given process. Iterates through an array of opcodes. Function also returns an integer to simulate handling interrupts
+//depending on the given opcode. Function takes in a time quantum to help simulate Round Robin Scheduling of CPU.
+	public int execute(int time, Queue qR, Queue qJ, Queue qIO, Semaphore semaph) throws InterruptedException {
+
+		long start = System.currentTimeMillis();
+
+		if (this.start == 0) {
+			this.start = System.currentTimeMillis();
+		}
+		// ...
+
+		int operation = 0;
+		System.out.println("Executing Process " + this.pid);
+
+		this.state = 2; // running state
+
+		start: for (int i = this.pc; programStart[i] != 0; i++) {
+			operation = this.programStart[i] / 10000;
+			switch (operation) {
+			// "calculate" Opcode
+			case 1:
+				Thread.sleep(1); // sleep 1 second
+				break;
+
+			// "I/O" Opcode
+			case 2:
+				this.state = 3;
+				this.pc = i + 1;
+				qIO.queueEnqueue(this);
+				System.out.println("Process with pid:" + this.pid + " WAITING\n");
+				return (1);
+
+			// Criitical section entered
+			case 5:
+				// System.out.println("Process: "+ this.pid+ "sh");
+
+				switch (this.programStart[i] % 10) {
+
+				case 0:
+					System.out.println("Process: " + this.pid + " Entering Critical Section; ");
+					// Semaphore.queue.add(this.pid);
+					// this.pc = i++;
+					// if(Semaphore.numUsed==Semaphore.numAllowed) {
+					// Semaphore.mutexLock = true;
+					// System.out.println("MAX Critical Section reached");
+
+					break;
+
+				case 1:
+					// Semaphore.numUsed--;
+					// if(Semaphore.numUsed<Semaphore.numAllowed) Semaphore.mutexLock = false;
+					System.out.println("Process: " + this.pid + " Leaving Critical Section");
+					break;
+
+				}
+
+				// Fork opcode
+			case 4:
+				this.state = 3;
+				this.pc = i + 1;
+				qR.queueEnqueue(this);
+				return (2);
+			// generate process and execute it
+
+			}
+
+			long finish = System.currentTimeMillis();
+			long timeElapsed = finish - start;
+			this.end = System.currentTimeMillis();
+
+			// System.out.println("time elapsed:" + timeElapsed);
+
+			// if timer runs out, save pc, change state to ready, enqueue current PCB to
+			// ready queue
+			if ((timeElapsed) > time) {
+				this.pc++;
+				this.state = 2;
+				qR.queueEnqueue(this);
+				return 2;
+
+			}
+		}
+		main m = new main();
+		System.out.println("Process with pid:" + this.pid + " terminated\n");
+
+		main.turnarounds.add((int) (this.end - this.start));
+
+		this.state = 5;
+
+		return 0;
+
+	}
+
+	public void printMemory() {
+		System.out.println("Printing Memory for PID: " + this.pid);
+		for (int i = 0; this.programStart[i] != 0; i++) {
+			System.out.println(programStart[i]);
+		}
+		return;
+
+	}
 }
--- OS_Simulator/src/kernel/main.java
+++ OS_Simulator/src/kernel/main.java
@@ -6,167 +6,153 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
-	
-	//List of turnaround times
+
+	// List of turnaround times
 	public static ArrayList<Integer> turnarounds = new ArrayList<Integer>();
-	
-	//list of wait times
+
+	// list of wait times
 	public static ArrayList<Integer> waits = new ArrayList<Integer>();
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		//System.out.println(System.getProperty("user.dir"));
-		
-	
-		System.out.println("Welcome to Death Megatron OS");
+		// System.out.println(System.getProperty("user.dir"));
+
+		System.out.println("Welcome to my Operating System");
 		System.out.println("Booting System...");
-		
+
 		int TIME_QUANTUM = 10;
 		PCBGenerator generator = new PCBGenerator();
-		String[] commands = {"generate", "run", "clean", "stop"};
+		String[] commands = { "generate", "run", "clean", "stop" };
 		String command;
 		boolean run = false;
-		//Scanner scan = new Scanner(System.in);
+		// Scanner scan = new Scanner(System.in);
 		int number;
-		
-		while(true) {
+
+		while (true) {
 			Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a command");
-		command = scan.nextLine();
-		
-		
-		switch(command.charAt(0)) {
-		case 'g':
-			System.out.println("Enter the number of programs you would like to generate");
-			number  = scan.nextInt();
-			
-			System.out.println("Generating files..");
-			for(int i =0; i<number;i++) {
-				generator.generateTXT();
-			}
-			break;
-			
-		case 'r':
-			do {
-				
-				PCB currentlyRunning;
-				
-				int pCounter = 0;		
-			
-				
-				
-				long IO_WAIT = 10;
-				
-				//Open Program folder and parse txt files
-				final File programs = new File("src\\kernel\\Programs");
-				
-				Queue jobQueue = new Queue(999);
-				Queue readyQueue = new Queue(999);
-				Queue IOQueue = new Queue(999);
-				
-				
-				for (File fileEntry : programs.listFiles()) {
-					PCB block = new PCB();
-					
-					//System.out.println("Opening file for process: "+ block.pid);
-					block.loadToMemory(fileEntry);
-					block.state = 2;
-					//block.printMemory();
-					readyQueue.queueEnqueue(block);
-					pCounter++;
-					continue;
-			    }
-				
-				//System.out.println("Printing Job Queue");
-				//jobQueue.queueDisplay();
-				
-			    //System.out.println("Printing Ready Queue");
-				//readyQueue.queueDisplay();
-				
-				Semaphore semaph = new Semaphore(2);
-				
-				
-				System.out.println("Round Robin Execution");
-				
-				long RRStart = System.currentTimeMillis();
-				
-				
-				//Iterate through read queue and execute programs unitl empty (RR executions)
-				finish:
-				while(readyQueue.getSize()!=0) {
-				long start = System.currentTimeMillis();
-				int status = readyQueue.queueDequeue().execute(10, jobQueue, readyQueue, IOQueue, semaph);
-				
-				switch(status) {
-				case 0:
-					
-					
-					continue;
-				
-				case 1:
-					long finish = System.currentTimeMillis();
-					long timeElapsed = finish - start;
-					if(readyQueue.getSize()>0) {
-						while(status != 1 || timeElapsed < IO_WAIT) {
-							
-						try {
-						status = readyQueue.queueDequeue().execute(TIME_QUANTUM, jobQueue, readyQueue, IOQueue, semaph);
-						}catch (NullPointerException e) {
-							break finish;
+			System.out.println("Enter a command");
+			command = scan.nextLine();
+
+			switch (command.charAt(0)) {
+			case 'g':
+				System.out.println("Enter the number of programs you would like to generate");
+				number = scan.nextInt();
+
+				System.out.println("Generating files..");
+				for (int i = 0; i < number; i++) {
+					generator.generateTXT();
+				}
+				break;
+
+			case 'r':
+				do {
+
+					PCB currentlyRunning;
+
+					int pCounter = 0;
+
+					long IO_WAIT = 10;
+
+					// Open Program folder and parse txt files
+					final File programs = new File("src\\kernel\\Programs");
+
+					Queue jobQueue = new Queue(999);
+					Queue readyQueue = new Queue(999);
+					Queue IOQueue = new Queue(999);
+
+					for (File fileEntry : programs.listFiles()) {
+						PCB block = new PCB();
+
+						// System.out.println("Opening file for process: "+ block.pid);
+						block.loadToMemory(fileEntry);
+						block.state = 2;
+						// block.printMemory();
+						readyQueue.queueEnqueue(block);
+						pCounter++;
+						continue;
+					}
+
+					// System.out.println("Printing Job Queue");
+					// jobQueue.queueDisplay();
+
+					// System.out.println("Printing Ready Queue");
+					// readyQueue.queueDisplay();
+
+					Semaphore semaph = new Semaphore(2);
+
+					System.out.println("Round Robin Execution");
+
+					long RRStart = System.currentTimeMillis();
+
+					// Iterate through read queue and execute programs unitl empty (RR executions)
+					finish: while (readyQueue.getSize() != 0) {
+						long start = System.currentTimeMillis();
+						int status = readyQueue.queueDequeue().execute(10, jobQueue, readyQueue, IOQueue, semaph);
+
+						switch (status) {
+						case 0:
+
+							continue;
+
+						case 1:
+							long finish = System.currentTimeMillis();
+							long timeElapsed = finish - start;
+							if (readyQueue.getSize() > 0) {
+								while (status != 1 || timeElapsed < IO_WAIT) {
+
+									try {
+										status = readyQueue.queueDequeue().execute(TIME_QUANTUM, jobQueue, readyQueue,
+												IOQueue, semaph);
+									} catch (NullPointerException e) {
+										break finish;
+									}
+								}
+							}
+							System.out.println("Continuing Process with Pid: " + readyQueue.queue[0].pid);
+							status = IOQueue.queueDequeue().execute(TIME_QUANTUM, jobQueue, readyQueue, IOQueue,
+									semaph);
+							break;
+						case 2:
+							System.out.println("Process ^ FORKED\n");
+							generator.forkPCB().execute(TIME_QUANTUM, jobQueue, readyQueue, IOQueue, semaph);
+							continue;
 						}
 					}
-					}
-					System.out.println("Continuing Process with Pid: "+ readyQueue.queue[0].pid );
-					status = IOQueue.queueDequeue().execute(TIME_QUANTUM, jobQueue, readyQueue, IOQueue, semaph);
-					break;
-				case 2:
-					System.out.println("Process ^ FORKED\n" );
-					generator.forkPCB().execute(TIME_QUANTUM, jobQueue, readyQueue, IOQueue, semaph);
-					continue;
-				}
-				}
-				int tavg = 0;
-				
-				tavg /= main.turnarounds.size();
-				long RRFinish = System.currentTimeMillis();
-				System.out.println("Round Robin Executuon Stats:-----------\n"
-						+ "Time Complemeted: "+ (RRFinish - RRStart) + "\n"
-								+ "Average Turnaround Time: \n"  
-								+ "Average Wait Time: \n");
-				
-				//System.out.println("Printing IO Queue");
-				
-				//IOQueue.queueDisplay();
-				
-				
-				System.out.println("would you like to run again? Y/N");
-				if(scan.next().charAt(0)=='y') run =  true;
-				else run = false;
-				
-				
-				
-			}while(run);
-			System.out.println("Finishing Execution...");
-			break;
-			
-		case 'c':
-			generator.cleanFolder();
-			break;
-			
-		case 's':
-			System.out.println("Closing Program");
-			return;
-			
-		default:
-			System.out.println("Thats not a command sorry :(\n");
-		
-		
+					int tavg = 0;
+
+					tavg /= main.turnarounds.size();
+					long RRFinish = System.currentTimeMillis();
+					System.out.println("Round Robin Executuon Stats:-----------\n" + "Time Complemeted: "
+							+ (RRFinish - RRStart) + "\n" + "Average Turnaround Time: \n" + "Average Wait Time: \n");
+
+					// System.out.println("Printing IO Queue");
+
+					// IOQueue.queueDisplay();
+
+					System.out.println("would you like to run again? Y/N");
+					if (scan.next().charAt(0) == 'y')
+						run = true;
+					else
+						run = false;
+
+				} while (run);
+				System.out.println("Finishing Execution...");
+				break;
+
+			case 'c':
+				generator.cleanFolder();
+				break;
+
+			case 's':
+				System.out.println("Closing Program");
+				return;
+
+			default:
+				System.out.println("Thats not a command sorry :(\n");
+
+			}
+
 		}
-
-		
 	}
-	}
-		
-
 
 }
